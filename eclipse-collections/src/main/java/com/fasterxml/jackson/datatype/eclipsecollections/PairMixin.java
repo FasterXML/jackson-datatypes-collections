7fe4fa4d5023993aa949602eda31a6adb8c786bf
yawkat <jonas.konrad@oracle.com>
2022-03-17 15:45:12 +0100

    Fix serialization of pair/triple classes This broke in eclipse-collections 11 with the introduction of the `isSame` and `isEqual` methods on the pair/triple classes. This patch adds mixins to those classes that use `@JsonIncludeProperties` to only allow the expected properties.
    Fixes #94

---
+++ eclipse-collections/src/main/java/com/fasterxml/jackson/datatype/eclipsecollections/PairMixin.java
@@ -0,0 +1,7 @@
+package com.fasterxml.jackson.datatype.eclipsecollections;
+
+import com.fasterxml.jackson.annotation.JsonIncludeProperties;
+
+@JsonIncludeProperties({"one", "two"})
+class PairMixin {
+}
