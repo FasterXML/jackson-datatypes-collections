be8e8579e0129b3d7f033df6f205b23266702797
Tatu Saloranta <tatu.saloranta@iki.fi>
2023-02-28 20:30:24 -0800

    ...

--- guava/src/test/java/com/fasterxml/jackson/datatype/guava/MultiMap104Test.java
+++ guava/src/test/java/com/fasterxml/jackson/datatype/guava/MultiMap104Test.java
@@ -2,24 +2,20 @@ package com.fasterxml.jackson.datatype.guava;
 
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import com.fasterxml.jackson.databind.*;
-import com.fasterxml.jackson.databind.ObjectMapper.DefaultTyping;
-import com.fasterxml.jackson.databind.jsontype.impl.LaissezFaireSubTypeValidator;
 import com.google.common.collect.ArrayListMultimap;
 
 public class MultiMap104Test extends ModuleTestBase
 {
     // [datatypes-collections#104]
     static class Outside104 {
+        @JsonTypeInfo(include = JsonTypeInfo.As.PROPERTY, use = JsonTypeInfo.Id.CLASS)
         public Object aProperty;
     }
 
     // [datatypes-collections#104]
     public void testPolymorphicArrayMap() throws Exception
     {
-         final ObjectMapper mapper = builderWithModule()
-              .activateDefaultTyping(LaissezFaireSubTypeValidator.instance,
-                      DefaultTyping.NON_FINAL, JsonTypeInfo.As.PROPERTY)
-              .build();
+         final ObjectMapper mapper = mapperWithModule();
          final ArrayListMultimap<String,Object> multimap = ArrayListMultimap.create();
          multimap.put("aKey", 1);
          final Outside104 outside = new Outside104();
