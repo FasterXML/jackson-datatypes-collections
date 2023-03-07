56170d91e6f2a7ea7c30414f1bd65ea7120fd960
Tatu Saloranta <tatu.saloranta@iki.fi>
2023-03-06 20:26:43 -0800

    Update release notes wrt #102, minor streamlining

--- guava/src/test/java/com/fasterxml/jackson/datatype/guava/RangeDeserializer102Test.java
+++ guava/src/test/java/com/fasterxml/jackson/datatype/guava/RangeDeserializer102Test.java
@@ -6,18 +6,31 @@ import com.fasterxml.jackson.databind.exc.InvalidFormatException;
 import com.google.common.collect.BoundType;
 import com.google.common.collect.Range;
 
-public class RangeDeserializer102Test extends ModuleTestBase {
+public class RangeDeserializer102Test extends ModuleTestBase
+{
+    private final ObjectMapper MAPPER_DEFAULT = mapperWithModule();
+    private final ObjectMapper MAPPER_CASE_INSENSITIVE = builderWithModule()
+            .enable(MapperFeature.ACCEPT_CASE_INSENSITIVE_ENUMS)
+            .build();
 
-    public void testBoundTypeIsOnlyOneOfOpenOrClosedInUpperCase() {
-        assertEquals(2, BoundType.values().length);
-        assertEquals("OPEN", BoundType.values()[0].name());
-        assertEquals("CLOSED", BoundType.values()[1].name());
+    public void testDeserializeDefaultSuccess() throws Exception
+    {
+        _testDeserializeOk(MAPPER_DEFAULT, "CLOSED", "OPEN");
     }
 
-    public void testDeserializeAcceptCaseInsensitiveBoundTypeSuccess() throws Exception {
-        String json = a2q("{'lowerEndpoint': 1, 'lowerBoundType': 'closed', 'upperEndpoint': 2, 'upperBoundType': 'oPeN'}");
-        ObjectMapper mapper = mapperWithModule().enable(MapperFeature.ACCEPT_CASE_INSENSITIVE_ENUMS);
+    public void testDeserializeAcceptCaseInsensitiveBoundTypeSuccess() throws Exception
+    {
+        _testDeserializeOk(MAPPER_CASE_INSENSITIVE, "CLOSED", "OPEN");
+        _testDeserializeOk(MAPPER_CASE_INSENSITIVE, "closed", "OPEN");
+        _testDeserializeOk(MAPPER_CASE_INSENSITIVE, "CLOSED", "open");
+        _testDeserializeOk(MAPPER_CASE_INSENSITIVE, "Closed", "Open");
+        _testDeserializeOk(MAPPER_CASE_INSENSITIVE, "ClOseD", "opEN");
+    }
 
+    private void _testDeserializeOk(ObjectMapper mapper,
+            String lowerType, String upperType) throws Exception {
+        String json = a2q("{'lowerEndpoint': 1, 'lowerBoundType': '"+lowerType
+                +"', 'upperEndpoint': 2, 'upperBoundType': '"+upperType+"'}");
         Range range = mapper.readValue(json, Range.class);
 
         assertEquals(1, range.lowerEndpoint());
@@ -28,27 +41,23 @@ public class RangeDeserializer102Test extends ModuleTestBase {
 
     public void testDeserializeBoundTypeFailWithFirstInvalidValue() throws Exception {
         String json = a2q("{'lowerEndpoint': 1, 'lowerBoundType': 'closed', 'upperEndpoint': 2, 'upperBoundType': 'oPeN'}");
-        ObjectMapper mapper = mapperWithModule().disable(MapperFeature.ACCEPT_CASE_INSENSITIVE_ENUMS);
-
         try {
-            mapper.readValue(json, Range.class);
+            MAPPER_DEFAULT.readValue(json, Range.class);
             fail("should not pass");
         } catch (InvalidFormatException e) {
             verifyException(e, "Cannot deserialize value of type `com.google.common.collect.BoundType` from " +
-                "String \"closed\": not a valid BoundType name (should be one oF: [OPEN, CLOSED])");
+                "String \"closed\": not a valid BoundType name (should be one of: [OPEN, CLOSED])");
         }
     }
 
     public void testDeserializeBoundTypeFailWithFirstInvalidValueFlip() throws Exception {
         String json = a2q("{'upperEndpoint': 2, 'upperBoundType': 'oPeN', 'lowerEndpoint': 1, 'lowerBoundType': 'closed'}");
-        ObjectMapper mapper = mapperWithModule().disable(MapperFeature.ACCEPT_CASE_INSENSITIVE_ENUMS);
-
         try {
-            mapper.readValue(json, Range.class);
+            MAPPER_DEFAULT.readValue(json, Range.class);
             fail("should not pass");
         } catch (InvalidFormatException e) {
             verifyException(e, "Cannot deserialize value of type `com.google.common.collect.BoundType` from " +
-                "String \"oPeN\": not a valid BoundType name (should be one oF: [OPEN, CLOSED])");
+                "String \"oPeN\": not a valid BoundType name (should be one of: [OPEN, CLOSED])");
         }
     }
 }
