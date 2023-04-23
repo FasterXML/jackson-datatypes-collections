5b1001346d5a3aecc46b98b7d94df3bad5fcf6a6
joohyukkim <beanskobe@gmail.com>
2023-04-23 23:58:44 +0900

    Move to passing tests

--- guava/src/test/java/com/fasterxml/jackson/datatype/guava/JsonDeserContentConverter92Test.java
+++ guava/src/test/java/com/fasterxml/jackson/datatype/guava/JsonDeserContentConverter92Test.java
@@ -1,4 +1,4 @@
-package com.fasterxml.jackson.datatype.guava.failing;
+package com.fasterxml.jackson.datatype.guava;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
