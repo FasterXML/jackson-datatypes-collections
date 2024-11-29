614240a6234a90498ac9a124e6f00e08418e8ba5
Tatu Saloranta <tatu.saloranta@iki.fi>
2024-11-29 14:53:14 -0800

    Optimize tests slightly

--- eclipse-collections/src/test/java/tools/jackson/datatype/eclipsecollections/SerializerTest.java
+++ eclipse-collections/src/test/java/tools/jackson/datatype/eclipsecollections/SerializerTest.java
@@ -28,53 +28,79 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public final class SerializerTest extends ModuleTestBase {
+    private final ObjectMapper MAPPER = mapperWithModule();
+
     @Test
     public void ref() throws IOException {
         Assert.assertEquals(
                 "[\"a\",\"b\",\"c\"]",
-                mapperWithModule().writeValueAsString(Sets.immutable.of("a", "b", "c"))
+                MAPPER.writeValueAsString(Sets.immutable.of("a", "b", "c"))
         );
     }
 
     @Test
     public void primitive() throws IOException {
-        Assert.assertEquals("[true,false,true]", mapperWithModule().writeValueAsString(
+        Assert.assertEquals("[true,false,true]", MAPPER.writeValueAsString(
                 BooleanLists.immutable.of(true, false, true)));
-        Assert.assertEquals("[1,2,3]", mapperWithModule().writeValueAsString(
+        Assert.assertEquals("[1,2,3]", MAPPER.writeValueAsString(
                 ShortLists.immutable.of((short) 1, (short) 2, (short) 3)));
-        Assert.assertEquals("[1,2,3]", mapperWithModule().writeValueAsString(
+        Assert.assertEquals("[1,2,3]", MAPPER.writeValueAsString(
                 IntLists.immutable.of(1, 2, 3)));
-        Assert.assertEquals("[1.1,2.3,3.5]", mapperWithModule().writeValueAsString(
+        Assert.assertEquals("[1.1,2.3,3.5]", MAPPER.writeValueAsString(
                 FloatLists.immutable.of(1.1F, 2.3F, 3.5F)));
-        Assert.assertEquals("[1,2,3]", mapperWithModule().writeValueAsString(
+        Assert.assertEquals("[1,2,3]", MAPPER.writeValueAsString(
                 LongLists.immutable.of(1, 2, 3)));
-        Assert.assertEquals("[1.1,2.3,3.5]", mapperWithModule().writeValueAsString(
+        Assert.assertEquals("[1.1,2.3,3.5]", MAPPER.writeValueAsString(
                 DoubleLists.immutable.of(1.1, 2.3, 3.5)));
 
         Assert.assertEquals(
-                mapperWithModule().writeValueAsString(new byte[]{ 1, 2, 3 }),
-                mapperWithModule().writeValueAsString(ByteLists.immutable.of((byte) 1, (byte) 2, (byte) 3)));
+                MAPPER.writeValueAsString(new byte[]{ 1, 2, 3 }),
+                MAPPER.writeValueAsString(ByteLists.immutable.of((byte) 1, (byte) 2, (byte) 3)));
         Assert.assertEquals(
-                mapperWithModule().writeValueAsString(new char[]{ '1', '2', '3' }),
-                mapperWithModule().writeValueAsString(CharLists.immutable.of('1', '2', '3')));
+                MAPPER.writeValueAsString(new char[]{ '1', '2', '3' }),
+                MAPPER.writeValueAsString(CharLists.immutable.of('1', '2', '3')));
         Assert.assertEquals(
-                mapperWithModule().writer().with(SerializationFeature.WRITE_CHAR_ARRAYS_AS_JSON_ARRAYS)
+                MAPPER.writer().with(SerializationFeature.WRITE_CHAR_ARRAYS_AS_JSON_ARRAYS)
                         .writeValueAsString(new char[]{ '1', '2', '3' }),
-                mapperWithModule().writer().with(SerializationFeature.WRITE_CHAR_ARRAYS_AS_JSON_ARRAYS)
+                        MAPPER.writer().with(SerializationFeature.WRITE_CHAR_ARRAYS_AS_JSON_ARRAYS)
                         .writeValueAsString(CharLists.immutable.of('1', '2', '3')));
     }
 
-    private void primitiveTypeSer(String data, PrimitiveIterable iterable)
-        throws IOException
+    @Test
+    public void primitiveTypeSer() throws Exception
     {
-        primitiveTypeSer(data, iterable, mapperWithModule());
+        primitiveTypeSer("[true,false,true]", BooleanLists.immutable.of(true, false, true));
+        primitiveTypeSer("[1,2,3]", ShortLists.immutable.of((short) 1, (short) 2, (short) 3));
+        primitiveTypeSer("[1,2,3]", IntLists.immutable.of(1, 2, 3));
+        primitiveTypeSer("[1.1,2.3,3.5]", FloatLists.immutable.of(1.1F, 2.3F, 3.5F));
+        primitiveTypeSer("[1,2,3]", LongLists.immutable.of(1, 2, 3));
+        primitiveTypeSer("[1.1,2.3,3.5]", DoubleLists.immutable.of(1.1, 2.3, 3.5));
+
+        primitiveTypeSer(
+                MAPPER.writeValueAsString(new byte[]{ 1, 2, 3 }),
+                ByteLists.immutable.of((byte) 1, (byte) 2, (byte) 3));
+        primitiveTypeSer(
+                MAPPER.writeValueAsString(new char[]{ '1', '2', '3' }),
+                CharLists.immutable.of('1', '2', '3'));
+        primitiveTypeSer(
+                MAPPER.writer().with(SerializationFeature.WRITE_CHAR_ARRAYS_AS_JSON_ARRAYS)
+                        .writeValueAsString(new char[]{ '1', '2', '3' }),
+                CharLists.immutable.of('1', '2', '3'),
+                mapperBuilder().configure(SerializationFeature.WRITE_CHAR_ARRAYS_AS_JSON_ARRAYS, true)
+                    .build()
+        );
     }
 
-    private void primitiveTypeSer(
-            String data,
-            PrimitiveIterable iterable,
-            ObjectMapper objectMapper
-    ) throws IOException {
+    private void primitiveTypeSer(String data, PrimitiveIterable iterable)
+            throws Exception
+    {
+        primitiveTypeSer(data, iterable, MAPPER);
+    }
+
+    private void primitiveTypeSer(String data, PrimitiveIterable iterable,
+            ObjectMapper objectMapper)
+        throws Exception
+    {
         class Wrapper {
             @JsonTypeInfo(use = JsonTypeInfo.Id.CLASS)
             Object object;
@@ -87,38 +113,13 @@ public final class SerializerTest extends ModuleTestBase {
     }
 
     @Test
-    public void primitiveTypeSer() throws IOException
-    {
-        primitiveTypeSer("[true,false,true]", BooleanLists.immutable.of(true, false, true));
-        primitiveTypeSer("[1,2,3]", ShortLists.immutable.of((short) 1, (short) 2, (short) 3));
-        primitiveTypeSer("[1,2,3]", IntLists.immutable.of(1, 2, 3));
-        primitiveTypeSer("[1.1,2.3,3.5]", FloatLists.immutable.of(1.1F, 2.3F, 3.5F));
-        primitiveTypeSer("[1,2,3]", LongLists.immutable.of(1, 2, 3));
-        primitiveTypeSer("[1.1,2.3,3.5]", DoubleLists.immutable.of(1.1, 2.3, 3.5));
-
-        primitiveTypeSer(
-                mapperWithModule().writeValueAsString(new byte[]{ 1, 2, 3 }),
-                ByteLists.immutable.of((byte) 1, (byte) 2, (byte) 3));
-        primitiveTypeSer(
-                mapperWithModule().writeValueAsString(new char[]{ '1', '2', '3' }),
-                CharLists.immutable.of('1', '2', '3'));
-        primitiveTypeSer(
-                mapperWithModule().writer().with(SerializationFeature.WRITE_CHAR_ARRAYS_AS_JSON_ARRAYS)
-                        .writeValueAsString(new char[]{ '1', '2', '3' }),
-                CharLists.immutable.of('1', '2', '3'),
-                mapperBuilder().configure(SerializationFeature.WRITE_CHAR_ARRAYS_AS_JSON_ARRAYS, true)
-                    .build()
-        );
-    }
-
-    @Test
     public void primitiveMaps() throws Exception
     {
         DeserializerTest.primitiveMaps0(mapperWithModule(), true);
     }
 
     @Test
-    public void objectObjectMaps() throws IOException {
+    public void objectObjectMaps() throws Exception {
         Assert.assertEquals(
                 "{\"abc\":\"def\"}",
                 mapperWithModule().writerFor(MutableMap.class).writeValueAsString(Maps.mutable.of("abc", "def"))
@@ -147,7 +148,7 @@ public final class SerializerTest extends ModuleTestBase {
     }
 
     @Test
-    public void typeInfoObjectMap() throws IOException {
+    public void typeInfoObjectMap() throws Exception {
         Assert.assertEquals(
                 "{\"map\":{\"0\":{\"@c\":\".SerializerTest$B\"}}}",
                 mapperWithModule().writeValueAsString(new Container())
